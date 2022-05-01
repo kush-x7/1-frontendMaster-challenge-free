5e37d478e817ea9b838ec12aa797e8c3f14d07d6
Kushagra Tripathi <kushagra.tripathi@squareboat.com>
2022-05-01 14:58:31 +0530

    add: copleted mobile section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        karla: ['Karla', 'sans-serif'],
+      },
+
+      colors: {
+        primary: {
+          Cyan: 'hsl(179, 62%, 43%)',
+          Yellow: 'hsl(71, 73%, 54%)',
+        },
+
+        neutral: {
+          LightGray: 'hsl(204, 43%, 93%)',
+          GrayishBlue: 'hsl(218, 22%, 67%)',
+        },
+      },
+    },
   },
+
   plugins: [],
 };
